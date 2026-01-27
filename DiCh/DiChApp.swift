7263df94dfc3e98ef40fb8afe6baa40125595344
dimonss <faimos69@gmail.com>
2026-01-27 13:44:55 +0600

    Initial Commit

---
+++ DiCh/DiChApp.swift
@@ -0,0 +1,32 @@
+//
+//  DiChApp.swift
+//  DiCh
+//
+//  Created by Chalysh Dmitrii on 27/1/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DiChApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DiCh/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DiCh
+//
+//  Created by Chalysh Dmitrii on 27/1/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
